7b701d8a8ed1e6891bc34b58cd840c9d0544ebe7
raviabreu8 <raviabreu8@gmail.com>
2026-06-10 18:45:49 +0100

    ppo_tanh_random_ablation_no_cliff_recovery_50k

--- thymio_rl_template/controllers/thymio_rl_controller/thymio_rl_controller.py
+++ thymio_rl_template/controllers/thymio_rl_controller/thymio_rl_controller.py
@@ -168,16 +168,16 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
         self.reward_function = ThymioReward(
             cell_size=0.20,
             survival_reward=0.01,
-            new_cell_reward=0.00,
-            revisit_penalty=0.00,
+            new_cell_reward=1.00,
+            revisit_penalty=-0.02,
             max_forward_reward=0.02,
             obstacle_penalty_scale=0.30,
             rotation_penalty_scale=0.02,
-            cliff_warning_penalty=-0.20,
-            ground_recovery_scale=3.00,
-            dangerous_forward_penalty_scale=2.00,
-            reverse_recovery_scale=0.20,
-            turn_away_scale=0.20,
+            cliff_warning_penalty=-0.00,
+            ground_recovery_scale=0.00,
+            dangerous_forward_penalty_scale=0.00,
+            reverse_recovery_scale=0.00,
+            turn_away_scale=0.00,
             collision_penalty=-50.00,
             fall_penalty=-150.00,
             cliff_penalty=-80.00
@@ -625,11 +625,11 @@ def main():
     only_evaluate = True
 
     ## Altera estes cinco valores entre experiências.
-    experiment_name = "ppo_tanh_random_ablation_no_exploration_50k"
+    experiment_name = "ppo_tanh_random_ablation_no_cliff_recovery_50k"
     model_type = "PPO"
     activation_name = "Tanh"
     environment_name = "random"
-    ablation_name = "no_exploration"
+    ablation_name = "no_cliff_recovery"
 
     total_training_steps = 50000
     max_episode_steps = 600
